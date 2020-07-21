fbebbab9d69a98b499c29a12d536728784aa0ca1
Andreas <andreas.vitikan@gmail.com>
2020-07-21 14:36:22 +0300

    Initial lines on the parser

--- parse.py
+++ parse.py
@@ -1,3 +1,5 @@
 import json
-f = read("date_21_iulie_la_13_00.json", "r")
-date_azi = json.loads(f.read())
+f = open("date_21_iulie_la_13_00.json", "r")
+latest = json.loads(f.read())
+print(latest['lasUpdatedOn'])
+
