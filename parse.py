650fc04b1d301f29ec68f655a053c66a4dc83675
Andreas <andreas.vitikan@gmail.com>
2020-07-21 21:43:29 +0300

    Done some work on the parser

--- parse.py
+++ parse.py
@@ -1,27 +1,20 @@
-#ToDo add additional functionality to check if the
-#JSON file for today has been issued (13:00), and if so
-#check if it exists in the data directory, and if not
-#download it and use that
-
 import json
-#from time import time, strftime
-import time
-from datetime import date
-import locale
-
-#set the locale to romanian
-locale.setlocale(locale.LC_ALL, "ro_RO.utf8")
-
-json_file_name = "date_21_iulie_la_13_00.json"
-f = open("data/" + json_file_name, "r")
+f = open("data/latestData.json", "r")
 latest = json.loads(f.read())
-print("The JSON file imported for analysis was last updated on: ")
-print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(latest['lasUpdatedOn'])))
+
+import datetime
+latest_timestamp = datetime.datetime.fromtimestamp(latest['lasUpdatedOn'])
+delta = datetime.datetime.now() - latest_timestamp
+if delta.seconds > 24*60*60:
+	import urllib.request
+	urllib.request.urlretrieve("https://datelazi.ro/latestData.json", "./latestData.json")
+else:
+	print("Fișierul JSON este în regulă!")
 
 currentDayStats = latest['currentDayStats']
-print("The total number of infected today is: ")
-print(currentDayStats['numberInfected'])
-print("The total number of deceased today is: ")
-print(currentDayStats['numberDeceased'])
+print("Datele au fost actualizate ultima oară: {}".format(latest['lasUpdatedOnString']))
+print("Numărul total de infectați: {}".format(currentDayStats['numberInfected']))
+print("Numărul total de decese: {}".format(currentDayStats['numberDeceased']))
+print("Numărul total de vindecați: {}".format(currentDayStats['numberCured']))
 
 
