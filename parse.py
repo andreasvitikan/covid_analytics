31aabc61b30c0568768c1ba4dfce7ce26e2d9812
Andreas <andreas.vitikan@gmail.com>
2020-07-21 15:25:40 +0300

    The parser now reads when the json file was last updated

--- parse.py
+++ parse.py
@@ -1,5 +1,15 @@
+#ToDo add additional functionality to check if the
+#JSON file for today has been issued (13:00), and if so
+#check if it exists in the data directory, and if not
+#download it and use that
+
 import json
-f = open("date_21_iulie_la_13_00.json", "r")
+import time
+
+json_file_name = "date_21_iulie_la_13_00.json"
+f = open("data/" + json_file_name, "r")
 latest = json.loads(f.read())
-print(latest['lasUpdatedOn'])
+print("The JSON file imported for analysis was last updated on: ")
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(latest['lasUpdatedOn'])))
+
 
