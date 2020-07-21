ddccd8c7bdaf51d6e311a6563c0666a246a5c3bd
Andreas <andreas.vitikan@gmail.com>
2020-07-21 21:58:06 +0300

    Added functional datetime iterator for historicalData

--- parse.py
+++ parse.py
@@ -17,4 +17,14 @@ print("Numărul total de infectați: {}".format(currentDayStats['numberInfected'
 print("Numărul total de decese: {}".format(currentDayStats['numberDeceased']))
 print("Numărul total de vindecați: {}".format(currentDayStats['numberCured']))
 
+# iterating through the last 7 days
+today = datetime.date.today()
+day = datetime.timedelta(days = 1)
+
+current_day = today
+for i in range(7):
+	current_day = current_day - day
+	current_day_string = current_day.strftime("%Y-%m-%d")
+	historicalData = latest['historicalData'][current_day_string]
+	print("This historicalData was parsed on: {}".format(historicalData['parsedOn']))
 
