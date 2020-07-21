c2cd5796295d1d6da1a2ada8ecaca9411db570a2
Andreas <andreas.vitikan@gmail.com>
2020-07-21 23:23:51 +0300

    Generates the number of infected each day

--- parse.py
+++ parse.py
@@ -6,8 +6,11 @@ import datetime
 latest_timestamp = datetime.datetime.fromtimestamp(latest['lasUpdatedOn'])
 delta = datetime.datetime.now() - latest_timestamp
 if delta.seconds > 24*60*60:
+	import os
+	os.rename("data/latestData.json", "data/latestData.json.old.{}".format(latest['lasUpdatedOn']))
 	import urllib.request
 	urllib.request.urlretrieve("https://datelazi.ro/latestData.json", "./latestData.json")
+	print("Fișierul JSON vechi a fost redenumit, iar cel nou a fost descărcat! Rulați programul din nou!")
 else:
 	print("Fișierul JSON este în regulă!")
 
@@ -18,13 +21,16 @@ print("Numărul total de decese: {}".format(currentDayStats['numberDeceased']))
 print("Numărul total de vindecați: {}".format(currentDayStats['numberCured']))
 
 # iterating through the last 7 days
-today = datetime.date.today()
-day = datetime.timedelta(days = 1)
+interval = datetime.timedelta(days = 1)
+date_a = datetime.date.today() - interval
+date_b = date_a - interval
+end_date = datetime.date(2020, 3, 17)
 
-current_day = today
-for i in range(7):
-	current_day = current_day - day
-	current_day_string = current_day.strftime("%Y-%m-%d")
-	historicalData = latest['historicalData'][current_day_string]
-	print("This historicalData was parsed on: {}".format(historicalData['parsedOn']))
+while date_a > end_date:
+	date_a_string = date_a.strftime("%Y-%m-%d")
+	date_b_string = date_b.strftime("%Y-%m-%d")
+	numar_cazuri_pozitive = latest['historicalData'][date_a_string]['numberInfected'] - latest['historicalData'][date_b_string]['numberInfected']
+	print("Infectați în data de {} este de {}".format(date_a_string, numar_cazuri_pozitive))
+	date_a = date_a - interval
+	date_b = date_b - interval
 
