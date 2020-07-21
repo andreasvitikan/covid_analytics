61742c5803fad247fd947a47809de0cb7103bdfd
Andreas <andreas.vitikan@gmail.com>
2020-07-21 23:37:15 +0300

    It now generates C, D, V for all dates and pretty-prints it

--- parse.py
+++ parse.py
@@ -16,9 +16,10 @@ else:
 
 currentDayStats = latest['currentDayStats']
 print("Datele au fost actualizate ultima oară: {}".format(latest['lasUpdatedOnString']))
-print("Numărul total de infectați: {}".format(currentDayStats['numberInfected']))
+print("Numărul total de confirmați: {}".format(currentDayStats['numberInfected']))
 print("Numărul total de decese: {}".format(currentDayStats['numberDeceased']))
 print("Numărul total de vindecați: {}".format(currentDayStats['numberCured']))
+print()
 
 # iterating through the last 7 days
 interval = datetime.timedelta(days = 1)
@@ -29,8 +30,10 @@ end_date = datetime.date(2020, 3, 17)
 while date_a > end_date:
 	date_a_string = date_a.strftime("%Y-%m-%d")
 	date_b_string = date_b.strftime("%Y-%m-%d")
-	numar_cazuri_pozitive = latest['historicalData'][date_a_string]['numberInfected'] - latest['historicalData'][date_b_string]['numberInfected']
-	print("Infectați în data de {} este de {}".format(date_a_string, numar_cazuri_pozitive))
+	infected = latest['historicalData'][date_a_string]['numberInfected'] - latest['historicalData'][date_b_string]['numberInfected']
+	deceased = latest['historicalData'][date_a_string]['numberDeceased'] - latest['historicalData'][date_b_string]['numberDeceased']
+	cured = latest['historicalData'][date_a_string]['numberCured'] - latest['historicalData'][date_b_string]['numberCured']
+	print("{} \tconfirmați \t{} \tdecedați \t{} \tvindecați \t{}".format(date_a_string, infected, deceased, cured))
 	date_a = date_a - interval
 	date_b = date_b - interval
 
