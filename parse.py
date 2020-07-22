98c0399c8317e8861befda42f9472403442e5746
Andreas <andreas.vitikan@gmail.com>
2020-07-22 08:41:35 +0300

    Working historicalData analysis + fixed bug when running it the next day before 1 PM

--- parse.py
+++ parse.py
@@ -4,6 +4,9 @@ latest = json.loads(f.read())
 
 import datetime
 latest_timestamp = datetime.datetime.fromtimestamp(latest['lasUpdatedOn'])
+start_date = datetime.date.fromtimestamp(latest['lasUpdatedOn'])
+day = datetime.timedelta(days = 1)
+
 delta = datetime.datetime.now() - latest_timestamp
 if delta.seconds > 24*60*60:
 	import os
@@ -20,11 +23,22 @@ print("Numărul total de confirmați: {}".format(currentDayStats['numberInfected
 print("Numărul total de decese: {}".format(currentDayStats['numberDeceased']))
 print("Numărul total de vindecați: {}".format(currentDayStats['numberCured']))
 print()
+print("Analiza datelor istorice")
+print()
 
-# iterating through the last 7 days
-interval = datetime.timedelta(days = 1)
-date_a = datetime.date.today() - interval
-date_b = date_a - interval
+# Historical data analysis block
+
+yesterday_string = (start_date - day).strftime("%Y-%m-%d")
+infected_last = currentDayStats['numberInfected'] - latest['historicalData'][yesterday_string]['numberInfected']
+cured_last = currentDayStats['numberCured'] - latest['historicalData'][yesterday_string]['numberCured']
+deceased_last = currentDayStats['numberDeceased'] - latest['historicalData'][yesterday_string]['numberDeceased']
+print("{} \tconfirmați \t{} \tdecedați \t{} \tvindecați \t{}".format(start_date.strftime("%Y-%m-%d"), infected_last, deceased_last, cured_last))
+
+# This iterates day by day through historicalData
+# and generates the numbers for each day except for
+# the latest day (currentDayStats)
+date_a = datetime.date.fromtimestamp(latest['lasUpdatedOn']) - day
+date_b = date_a - day
 end_date = datetime.date(2020, 3, 17)
 
 while date_a > end_date:
@@ -34,6 +48,6 @@ while date_a > end_date:
 	deceased = latest['historicalData'][date_a_string]['numberDeceased'] - latest['historicalData'][date_b_string]['numberDeceased']
 	cured = latest['historicalData'][date_a_string]['numberCured'] - latest['historicalData'][date_b_string]['numberCured']
 	print("{} \tconfirmați \t{} \tdecedați \t{} \tvindecați \t{}".format(date_a_string, infected, deceased, cured))
-	date_a = date_a - interval
-	date_b = date_b - interval
+	date_a = date_a - day
+	date_b = date_b - day
 
